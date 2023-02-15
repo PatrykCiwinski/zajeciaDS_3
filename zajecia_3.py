07aa41e32620ac732d96445eae5c73039374f526
Patryk <patryk.ciwinski@gmail.com>
2023-02-15 18:05:15 +0100

    while with if

--- zajecia_3.py
+++ zajecia_3.py
@@ -1,10 +1,20 @@
-letter= input("Podaj literę: ")
-i = 0
+# letter= input("Podaj literę: ")
+# i = 0
+#
+# while letter !="":
+#     print(letter)
+#     i+=1
+#     if i == 5:
+#         letter=input("Podaj kolejną: ")
+#         i =0
+# print("bye bye")
 
-while letter !="":
-    print(letter)
-    i+=1
-    if i == 5:
-        letter=input("Podaj kolejną: ")
-        i =0
-print("bye bye")
+st = "\nPodaj imie"
+st +="\n(Wpisz q aby zakończyć)"
+
+while True:
+    name = input(st)
+    if name == "q":
+        break
+
+    print(f"wpisano imię {name.title()}")
